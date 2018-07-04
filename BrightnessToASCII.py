243102072857b14ceb3d75a7edad9cb74368b09d
sathwik <www.msathwik11@gmail.com>
2018-07-04 18:12:22 +0530

    Convert brightness numbers to ASCII characters

---
+++ BrightnessToASCII.py
@@ -0,0 +1,40 @@
+from PIL import Image
+
+# RGB mappings
+
+def Average(RGB):
+	return (RGB[0]+RGB[1]+RGB[2])/3
+
+def Lightness(RGB):
+	return (max(RGB)+min(RGB))/2
+
+def Luminosity(RGB):
+	return 0.21*RGB[0] + 0.72*RGB[1] + 0.07*RGB[2]
+
+# scale down Brightness(0,255) to custom ASCII(0,64)
+def toASCII(b):
+	chars =  r'`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+	slope = 66/255
+	return chars[round(slope*b)]
+
+im = Image.open("ascii-pineapple.jpg")
+
+if im != None:
+	pix = im.load()
+	width, height = im.size
+	bright = [None]*(width*height)
+	for col in range(width):
+		for row in range(height):
+			bright[width*row + col] = Average(pix[col,row])
+
+	asciiMatrix = [None]*(width*height)
+	for col in range(width):
+		for row in range(height):
+			asciiMatrix[width*row + col] = toASCII(bright[width*row + col])
+
+	print("Successfully constructed ASCII matrix!")
+	print("ASCII matrix size: %d x %d "%(im.size[0],im.size[1]))
+	print("Iterating through pixel ASCII characters:")
+	for row in range(height):
+		for col in range(width):
+			print(asciiMatrix[width*row + col])
